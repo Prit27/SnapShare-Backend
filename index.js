9d6263d990819aa4f1789903e4850a3f47f60667
PRIT THAKKAR <pr360648@dal.ca>
2022-04-06 22:07:17 -0300

    port change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 //const { PORT } = require('./src/util/constants/constants');
-app.listen(process.env.PORT || 8081);
+app.listen(process.env.PORT || 8080);
 console.log('Server Started on port 8080')
 
 
